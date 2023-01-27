2851455fe09bf274e0760c6337fd6bf68455462b
TPHsMP <o.g.kcity@gmail.com>
2023-01-27 14:07:52 +0600

    Бот с записью в БД

---
+++ main.py
@@ -0,0 +1,89 @@
+from aiogram import Bot, Dispatcher, executor, types
+import mysql.connector
+from errno import errorcode
+from datetime import datetime
+
+from settings import TOKEN, database
+
+bot = Bot(token=TOKEN, parse_mode='HTML')
+dp = Dispatcher(bot)
+
+
+# рассчет суммы чисел айдшника пользователя
+def get_magic_number(user_id):
+    string = list(str(user_id))
+    magic_number = 0
+    for i in range(len(string)):
+        magic_number = magic_number + int(string[i])
+
+    if magic_number > 9:
+        magic_number = get_magic_number(magic_number)
+    return magic_number
+
+
+@dp.message_handler(commands=['start'])
+async def command_start(message: types.Message):
+    # database = mysql.connector.connect(
+    #     host='localhost',
+    #     port='3305',
+    #     user='root',
+    #     passwd='123456',
+    #     database='prattler_bot_db')
+
+    date = datetime.date(datetime.today())
+    time = datetime.strftime(datetime.today(), '%H:%M:%S')
+
+    user_id = message.from_user.id
+    first_name = message.from_user.first_name
+    last_name = message.from_user.last_name
+
+    magic_number = get_magic_number(user_id)
+    print(magic_number)
+
+    try:
+        if database:
+            print('DB is connected')
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print('Wrong user data!')
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print('DB not found!')
+        else:
+            print(err)
+
+    try:
+        cursor = database.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS visitors '
+                       '(user_id INTEGER(12), '
+                       'first_name VARCHAR(255), '
+                       'last_name VARCHAR(255), '
+                       'magic_number INTEGER(1), '
+                       'visit_date DATE NOT NULL, '
+                       'visit_time TIME NOT NULL)')
+        db_record = 'INSERT INTO visitors (user_id, ' \
+                    'first_name, ' \
+                    'last_name, ' \
+                    'magic_number, ' \
+                    'visit_date, ' \
+                    'visit_time) VALUES (%s, %s, %s, %s, %s, %s)'
+        values_for_db = (user_id, first_name, last_name, magic_number, date, time)
+        cursor.execute(db_record, values_for_db)
+        database.commit()
+        with open(r'log.txt', 'a') as file:
+            file.write('>>>> ' + str(first_name) + ' ' + str(last_name) + ' record done\n')
+        database.close()
+    except mysql.connector.Error as err:
+        print('Unable to make a record in db for some reasons, check errors below: ', err)
+        with open(r'log.txt', 'a') as file:
+            file.write('>>>> ' + str(first_name) + ' ' + str(last_name) + ' record error: ' + str(err) + '\n')
+    # finally:
+    #     if database:
+    #         database.close()
+
+    await bot.send_message(message.from_user.id,
+                           f'Hello {first_name} {last_name}! Your user ID is: {user_id} '
+                           f'and your magic number is: {magic_number}')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
