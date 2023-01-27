ca7d931b43ca496c747abaf1c5013a3e046fc10f
tphsmp <40296154+tphsmp@users.noreply.github.com>
2023-01-27 15:32:39 +0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,1 @@
 TOKEN = ''
-
-import mysql.connector
-database = mysql.connector.connect(
-    host='localhost',
-    port='3305',
-    user='root',
-    passwd='123456',
-    database='prattler_bot_db')
